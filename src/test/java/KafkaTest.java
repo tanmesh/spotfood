944f2235ed9c8ed8003a28ac3d44ca93dfc6aacb
Tanmesh <tanmeshnm@gmail.com>
2023-11-09 16:47:16 -0700

    adding yaml

--- src/test/java/KafkaTest.java
+++ src/test/java/KafkaTest.java
@@ -1,19 +1,12 @@
-import com.tanmesh.splatter.configuration.KafkaConsumerConfig;
-import com.tanmesh.splatter.utils.CustomKafkaConsumer;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.junit.Test;
-
-
 public class KafkaTest {
 
-  @Test
-  public void testingKafkaConsumer() {
-    CustomKafkaConsumer consumer = new CustomKafkaConsumer("topic-user", new KafkaConsumerConfig());
-    
-    ConsumerRecords<String, String> records = consumer.poll();
-    for (ConsumerRecord<String, String> record : records) {
-      System.out.println("Received message: " + record.value());
-    }
-  }
+//  @Test
+//  public void testingKafkaConsumer() {
+//    CustomKafkaConsumer consumer = new CustomKafkaConsumer("topic-user", new KafkaConsumerConfig());
+//
+//    ConsumerRecords<String, String> records = consumer.poll();
+//    for (ConsumerRecord<String, String> record : records) {
+//      System.out.println("Received message: " + record.value());
+//    }
+//  }
 }
